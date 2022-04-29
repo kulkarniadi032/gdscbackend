84a215fa3658d8215110abde57d2e190482be1e9
Aditya Kulkarni <kulkarniadi032@gmail.com>
2022-04-29 08:23:05 +0530

    code updated

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const doubts = require("./routes/doubt");
 const register = require("./routes/register");
 const login = require("./routes/login");
 const app = express();
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(_dirname, "build", "index.html"));
-});
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cors());
 app.use("/api/v1", doubts);
 app.use("/api/v1", register);
 app.use("/api/v1", login);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(_dirname, "build", "index.html"));
+});
 app.listen(process.env.PORT, () => {
   console.log("server is running");
 });
